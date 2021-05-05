607e72439e115007a3a5f04112fbbb1c348bac11
Vetrivelu Murugesan <vetrivelu25@gmail.com>
2021-05-05 21:42:38 +0530

    first commit

---
+++ lib/src/pages/orders_history.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:mvc_pattern/mvc_pattern.dart';
+
+import '../../generated/l10n.dart';
+import '../controllers/order_controller.dart';
+import '../elements/EmptyOrdersWidget.dart';
+import '../elements/OrderItemWidget.dart';
+import '../elements/ShoppingCartButtonWidget.dart';
+
+class OrdersHistoryWidget extends StatefulWidget {
+  final GlobalKey<ScaffoldState> parentScaffoldKey;
+
+  OrdersHistoryWidget({Key key, this.parentScaffoldKey}) : super(key: key);
+
+  @override
+  _OrdersHistoryWidgetState createState() => _OrdersHistoryWidgetState();
+}
+
+class _OrdersHistoryWidgetState extends StateMVC<OrdersHistoryWidget> {
+  OrderController _con;
+
+  _OrdersHistoryWidgetState() : super(OrderController()) {
+    _con = controller;
+  }
+
+  @override
+  void initState() {
+    _con.listenForOrdersHistory();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context).copyWith(dividerColor: Colors.transparent);
+    double monthlyEarnings = 0;
+    _con.orders.forEach((order) {
+      monthlyEarnings += order.deliveryFee;
+    });
+    return Scaffold(
+      key: _con.scaffoldKey,
+      appBar: AppBar(
+        leading: new IconButton(
+          icon: new Icon(Icons.sort, color: Theme.of(context).hintColor),
+          onPressed: () => widget.parentScaffoldKey.currentState.openDrawer(),
+        ),
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+        title: Text(
+          S.of(context).orders_history,
+          style: Theme.of(context).textTheme.headline6.merge(TextStyle(letterSpacing: 1.3)),
+        ),
+        actions: <Widget>[
+          new ShoppingCartButtonWidget(iconColor: Theme.of(context).hintColor, labelColor: Theme.of(context).accentColor),
+        ],
+      ),
+      body: RefreshIndicator(
+        onRefresh: _con.refreshOrdersHistory,
+        child: ListView(
+          shrinkWrap: true,
+          primary: true,
+          padding: EdgeInsets.symmetric(vertical: 10),
+          children: <Widget>[
+            ListTile(
+              contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              leading: Icon(
+                Icons.account_balance_wallet_outlined,
+                color: Theme.of(context).hintColor,
+              ),
+              title: Text(
+                "Monthly Earnings : ${monthlyEarnings.toString()}",
+                style: Theme.of(context).textTheme.headline4,
+              ),
+            ),
+            _con.orders.isEmpty
+                ? EmptyOrdersWidget()
+                : ListView.separated(
+                    scrollDirection: Axis.vertical,
+                    shrinkWrap: true,
+                    primary: false,
+                    itemCount: _con.orders.length,
+                    itemBuilder: (context, index) {
+                      var _order = _con.orders.elementAt(index);
+                      return OrderItemWidget(expanded: index == 0 ? true : false, order: _order);
+                    },
+                    separatorBuilder: (context, index) {
+                      return SizedBox(height: 20);
+                    },
+                  ),
+          ],
+        ),
+      ),
+    );
+  }
+}
